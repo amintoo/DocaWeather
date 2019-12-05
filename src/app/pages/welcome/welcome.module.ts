f83f4619224b7b61e8650602b3e5d6920f997264
amintoo <amin.daher@outlook.fr>
2019-12-05 17:57:04 +0100

    projet fonctionnel

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
-import { LoginComponent } from '../../login/login.component';
 
 
 @NgModule({
   imports: [WelcomeRoutingModule],
-  declarations: [WelcomeComponent, LoginComponent],
+  declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
 export class WelcomeModule { }
